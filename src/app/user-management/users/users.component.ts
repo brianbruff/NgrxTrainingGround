606915405d6a0aee36be84370e90c3f8081f736a
Brian Keating <brian.keating@enverus.com>
2020-03-20 21:53:55 +0000

    chore: working on userId selector

--- src/app/user-management/users/users.component.ts
+++ src/app/user-management/users/users.component.ts
@@ -1,4 +1,47 @@
 import { Component, OnInit } from '@angular/core';
+import {ActionReducerMap, createFeatureSelector, createSelector} from '@ngrx/store';
+
+import * as fromUser from '../reducers/user.reducer';
+
+
+
+export interface State {
+  users: fromUser.State;
+}
+
+export const reducers: ActionReducerMap<State> = {
+  users: fromUser.reducer,
+};
+
+export const selectUserState = createFeatureSelector<fromUser.State>('users');
+
+export const selectUserIds = createSelector(
+  selectUserState,
+  fromUser.selectUserIds // shorthand for usersState => fromUser.selectUserIds(usersState)
+);
+export const selectUserEntities = createSelector(
+  selectUserState,
+  fromUser.selectUserEntities
+);
+export const selectAllUsers = createSelector(
+  selectUserState,
+  fromUser.selectAllUsers
+);
+export const selectUserTotal = createSelector(
+  selectUserState,
+  fromUser.selectUserTotal
+);
+export const selectCurrentUserId = createSelector(
+  selectUserState,
+  userState => userState.selectedUserId
+);
+
+export const selectCurrentUser = createSelector(
+  selectUserEntities,
+  selectCurrentUserId,
+  (userEntities, userId) => userEntities[userId]
+);
+
 
 @Component({
   selector: 'app-users',
