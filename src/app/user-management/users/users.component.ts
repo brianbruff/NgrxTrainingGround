f99def2a30dfc3f0ac0459035777b4e69558b1e1
Brian Keating <b@briankeating.net>
2020-03-21 21:17:39 +0000

    chore: implemented delete user

--- src/app/user-management/users/users.component.ts
+++ src/app/user-management/users/users.component.ts
@@ -1,35 +1,39 @@
 import { Component, OnInit } from '@angular/core';
-import {Observable} from "rxjs";
-import {UserModel} from "../models/user.model";
-import {Store} from "@ngrx/store";
+import {Observable} from 'rxjs';
+import {UserModel} from '../models/user.model';
+import {Store} from '@ngrx/store';
 import * as fromUsers from '../reducers/user.reducers';
 import * as userActions from '../actions/user.actions';
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
-  styleUrls: ['./users.component.css']
+  styleUrls: ['./users.component.scss']
 })
 export class UsersComponent implements OnInit {
 
-  users$ : Observable<UserModel[]>;
+  users$: Observable<UserModel[]>;
 
   constructor(private store: Store<fromUsers.State>) { }
 
   ngOnInit(): void {
-    this.users$ = this.store.select(fromUsers.selectAllUsers)
+    this.users$ = this.store.select(fromUsers.selectAllUsers);
   }
 
 
   createUser() {
-    const id = new Date().getUTCMilliseconds().toString()
+    const id = new Date().getUTCMilliseconds().toString();
     const user: UserModel = {
       id,
       name: 'test' + id
-    }
+    };
 
-    this.store.dispatch(userActions.addUser({user: user}))
+    this.store.dispatch(userActions.addUser({user}));
 
 
+  }
+
+  deleteUser(id: string) {
+    this.store.dispatch(userActions.deleteUser({id}));
   }
 }
